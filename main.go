6b8ddde76fe774fda0098aa45e3ecccd54f37b07
Sergey <lazybark.dev@gmail.com>
2022-08-26 19:29:49 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/lazybark/go-helpers/mock"
+)
+
+func main() {
+	w := mock.New()
+
+	w.HeaderData["one"] = []string{"one", "one"}
+	fmt.Println("Header is: ", w.HeaderData)
+	w.Write([]byte{'s', 'o', 'm', 'e', '_', 'd', 'a', 't', 'a'})
+	fmt.Println("Data is: ", *w.Data)
+	w.WriteHeader(1)
+	fmt.Println("Code is: ", *w.StatusCode)
+	w.Flush()
+	fmt.Println("Data is: ", *w.Data)
 }
