8d87524e57f8ad5b809d584fcd2560e4dedf6afc
Sergey <lazybark.dev@gmail.com>
2022-09-02 18:54:09 +0300

    Add ConvertCSVFiletoJSON

--- main.go
+++ main.go
@@ -2,19 +2,25 @@ package main
 
 import (
 	"fmt"
+	"log"
 
-	"github.com/lazybark/go-helpers/mock"
+	"github.com/lazybark/go-helpers/conv"
+	"github.com/lazybark/go-helpers/fsw"
 )
 
 func main() {
-	w := mock.New()
+	csv, converted, err := conv.ConvertCSVFiletoJSON("Namings copy.csv", ";", "Русский", "Английский", "Украинский")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("converted rows:", converted)
 
-	w.HeaderData["one"] = []string{"one", "one"}
-	fmt.Println("Header is: ", w.HeaderData)
-	w.Write([]byte{'s', 'o', 'm', 'e', '_', 'd', 'a', 't', 'a'})
-	fmt.Println("Data is: ", *w.Data)
-	w.WriteHeader(1)
-	fmt.Println("Code is: ", *w.StatusCode)
-	w.Flush()
-	fmt.Println("Data is: ", *w.Data)
+	f, err := fsw.MakePathToFile("wiki.json", true)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = f.Write(csv)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
