e78adc9a3da9cb227d38daac5a464611df922f4a
Sergey <lazybark.dev@gmail.com>
2023-06-27 13:26:10 +0300

    Update main.go

--- cmd/google-token/main.go
+++ cmd/google-token/main.go
@@ -3,5 +3,5 @@ package main
 import "github.com/lazybark/go-helpers/gapi"
 
 func main() {
-	gapi.GetTokenSheetsRead([]string{"https://www.googleapis.com/auth/spreadsheets.readonly"})
+	gapi.GetTokenSheetsRead([]string{"https://www.googleapis.com/auth/spreadsheets"})
 }
