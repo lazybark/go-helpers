75fade56c8d2aef13dc75d16d7afe846bbbd2599
Serhii <lazybark.dev@gmail.com>
2023-08-03 22:56:40 +0300

    Update hash_test.go

--- hasher/hash_test.go
+++ hasher/hash_test.go
@@ -12,10 +12,12 @@ func TestHash(t *testing.T) {
 	assert.Equal(t, "SHA256", SHA256.String())
 	assert.Equal(t, "SHA512", SHA512.String())
 	assert.Equal(t, "", HashType(9).String())
+	assert.Equal(t, "", HashType(-39).String())
 
 	assert.True(t, MD5.CheckType())
 	assert.True(t, SHA1.CheckType())
 	assert.True(t, SHA256.CheckType())
 	assert.True(t, SHA512.CheckType())
 	assert.False(t, HashType(19).CheckType())
+	assert.False(t, HashType(-3).CheckType())
 }
