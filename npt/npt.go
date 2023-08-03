c453ff389389dce76a7660da2197870249352839
Serhii <lazybark.dev@gmail.com>
2023-08-03 21:06:40 +0300

    NPT logic to epoch

--- npt/npt.go
+++ npt/npt.go
@@ -1,45 +1,48 @@
 package npt
 
-import "time"
+import (
+	"time"
+)
 
-//NPT is a time-holding object that does not have timezone pointer.
-//It's more memory-effective than default Go time.Time and can be used in apps that store time
-//in memory only and do not serialize it.
+// NPT is a time-holding object that does not have timezone pointer.
+// It's more memory-effective than default Go time.Time and can be used in apps that store time
+// in memory only and do not serialize it.
 //
-//For example: some action buffer that use time only for it's internal purposes of starting tasks
-//at specific moment.
+// For example: some action buffer that use time only for it's internal purposes of starting tasks
+// at specific moment.
+//
+// Important: NPT does not provide exact precision up to nano. Max precision is up to a second.
 type NPT struct {
-	sec  int64
-	nsec uint32
+	epoch int64
 }
 
-//Now creates NPT from current moment in time
+// Now creates NPT from current moment in time
 func Now() NPT {
-	var t NPT
-	t.sec = time.Now().Unix()
-	t.nsec = uint32(time.Now().UnixNano())
+	t := NPT{
+		epoch: time.Now().Unix(),
+	}
+
 	return t
 }
 
-//ToNow sets internal NPT values to current moment
+// ToNow sets internal NPT values to current moment
 func (t *NPT) ToNow() {
 	*t = Now()
 }
 
-//FromTime sets internal NPT values to specified time value
+// FromTime sets internal NPT values to specified time value
 func (t *NPT) FromTime(tm time.Time) {
-	t.sec = tm.Unix()
-	t.nsec = uint32(tm.UnixNano())
+	t.epoch = tm.Unix()
 }
 
-//ToNow sets internal NPT values to current moment
+// ToNow sets internal NPT values to current moment
 func (t *NPT) Add(d time.Duration) {
 	gt := t.Time()
-	gt.Add(d)
+	gt = gt.Add(d)
 	t.FromTime(gt)
 }
 
-//Time cunverts NPT into Time object
+// Time cunverts NPT into Time object
 func (t *NPT) Time() time.Time {
-	return time.Unix(t.sec, int64(t.nsec))
+	return time.Unix(t.epoch, 0)
 }
